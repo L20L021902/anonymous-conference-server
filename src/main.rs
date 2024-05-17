ab02195b0e5447e0921580d04697331b5e887836
L20L021902 <L20L021902@stu.hit.edu.cn>
2024-05-17 14:12:00 +0800

    feat: add --bind-address and --bind-port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::{error, info};
 use crate::protocol::enter_main_loop;
 
 mod constants;
@@ -16,6 +16,27 @@ async fn main() {
     env_logger::init();
     info!("AnonymousConference server");
     info!("Starting protocol manager...");
-    info!("Starting server on {}:{}", DEFAULT_BIND_ADDRESS, DEFAULT_PORT);
-    enter_main_loop(DEFAULT_BIND_ADDRESS.to_string(), DEFAULT_PORT).await;
+    let mut bind_address = DEFAULT_BIND_ADDRESS.to_string();
+    let mut bind_port = DEFAULT_PORT;
+    let mut args = std::env::args().skip(1); // skip binary name
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--bind-address" => {
+                if let Some(address) = args.next() {
+                    bind_address = address.to_string();
+                }
+            }
+            "--bind-port" => {
+                if let Some(port) = args.next() {
+                    bind_port = port.parse().unwrap();
+                }
+            }
+            _ => {
+                error!("Unknown argument: {}", arg);
+                return;
+            }
+        }
+    }
+    info!("Starting server on {}:{}", bind_address, bind_port);
+    enter_main_loop(bind_address, bind_port).await;
 }
